ca984c2b60a08e6c56b711fb08a9f56056080ecf
Thinira Nilushan Deshappriya <155083187+tn-deshappriya@users.noreply.github.com>
2025-07-12 23:44:11 +0530

    SignUp Activity UI Completed. (#5)
    * bugfix-2
    
    * completed SignIn UI.
    
    * create Navigation from signIn to Signup.
    
    * Created SignUp UI.

--- app/src/main/java/com/s23010467/easy_market/signup.java
+++ app/src/main/java/com/s23010467/easy_market/signup.java
@@ -1,8 +1,6 @@
 package com.s23010467.easy_market;
 
-
 import android.content.Intent;
-
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Shader;
@@ -17,43 +15,44 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class signin extends AppCompatActivity {
+public class signup extends AppCompatActivity {
 
-    // initialize multicolor Text variable
-    TextView multicolorText;
+    // Create a variable for signup multicolor..
+    TextView multicolor_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // Methord For Multicolor Text Signin...
+        // Asign values for variable...
 
-        multicolorText = findViewById(R.id.signin);
-        multicolorText.setText(getResources().getString(R.string.multicolor_signin_text));
+        multicolor_text = findViewById(R.id.signuptext);
+        multicolor_text.setText(getResources().getString(R.string.multicolor_signup_text));
 
-        TextPaint paint = multicolorText.getPaint();
-        float width = paint.measureText(getResources().getString(R.string.multicolor_signin_text));
-        Shader text_shader = new LinearGradient(0,0,width,multicolorText.getTextSize(),
+        // Method for Multicolor Text Sign Up..
+
+        TextPaint paint = multicolor_text.getPaint();
+        float width = paint.measureText(getResources().getString(R.string.multicolor_signup_text));
+        Shader text_shader = new LinearGradient(0,0,width,multicolor_text.getTextSize(),
                 new int[]{
                         Color.parseColor("#FF00E6"),
                         Color.parseColor("#931EFF"),
                         Color.parseColor("#3608ED"),
                 },null, Shader.TileMode.CLAMP);
-        multicolorText.getPaint().setShader(text_shader);
+        multicolor_text.getPaint().setShader(text_shader);
 
+        // Create navigation to signup to signin Activity page...
 
-        // create navigation in SignIn to SignUp Activity...
-
-        CardView signup = findViewById(R.id.signupBtn);
-        signup.setOnClickListener(v -> {
-            Intent intent = new Intent(signin.this, signup.class);
+        CardView signin = findViewById(R.id.signupSigninBtn);
+        signin.setOnClickListener(v -> {
+            Intent intent = new Intent(signup.this, signin.class);
             startActivity(intent);
         });
     }
