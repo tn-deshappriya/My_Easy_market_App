74d992dfe4170f9cdbf0f7f4b5978c9592c72770
Thinira Nilushan Deshappriya <155083187+tn-deshappriya@users.noreply.github.com>
2025-07-14 23:30:54 +0530

    Create Dashboard Header,Search,Time,Function Button Sections. (#7)
    * bugfix-2
    
    * completed SignIn UI.
    
    * create Navigation from signIn to Signup.
    
    * Created SignUp UI.
    
    * Create DashBoard Activity and set simple Navigation within SignIn,SignUp Activities.
    
    * SetUp Dashboard Head Section.
    
    * SetUp Dashboard Search Section.
    
    * SetUp Dashboard Announcement,Time,Main Function Button Sections.

--- app/src/main/java/com/s23010467/easy_market/dashboard.java
+++ app/src/main/java/com/s23010467/easy_market/dashboard.java
@@ -1,6 +1,8 @@
 package com.s23010467.easy_market;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,31 @@ public class dashboard extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        TextView announcementBanner = findViewById(R.id.announsment);
+        announcementBanner.setSelected(true);
+
+        String[] announcements = {
+            "Hello",
+                "Hi",
+                "I'm thinira Nilushan Deshappriya.\n I'm In Sooriyawewa.\n I'm Open University Student.\n I'm Software Engineering Student."
+        };
+        final int[] index = {0};
+
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (announcements.length==0){
+                    announcementBanner.setText("This Is Our Announcement Section\nAnd\nThis Time Not Have\nAnnouncements.");
+                }else {
+                    announcementBanner.setText(announcements[index[0]]);
+                    index[0] = (index[0] + 1) % announcements.length;
+                    handler.postDelayed(this, 4000); // Change every 4 seconds
+                }
+            }
+        };
+        handler.post(runnable);
     }
 }
