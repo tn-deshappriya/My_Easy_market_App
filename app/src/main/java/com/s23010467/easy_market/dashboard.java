fd4920ec33193a6239e2de0ae4593cfd39208ac2
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-07-23 03:17:57 +0530

    SetUp Dashboard Bottom Section to Navigate Home,Settings,Notifications and Change dashbord as a fragments.

--- app/src/main/java/com/s23010467/easy_market/dashboard.java
+++ app/src/main/java/com/s23010467/easy_market/dashboard.java
@@ -1,7 +1,11 @@
 package com.s23010467.easy_market;
 
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,6 +15,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class dashboard extends AppCompatActivity {
+
+    private int selectedTab =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,29 +30,139 @@ public class dashboard extends AppCompatActivity {
         });
 
 
-        TextView announcementBanner = findViewById(R.id.announsment);
-        announcementBanner.setSelected(true);
+        final LinearLayout homeLayout = findViewById(R.id.homeLayout);
+        final LinearLayout settingsLayout = findViewById(R.id.settingsLayout);
+        final LinearLayout notificationLayout = findViewById(R.id.notificationLayout);
 
-        String[] announcements = {
-            "Hello",
-                "Hi",
-                "I'm thinira Nilushan Deshappriya.\n I'm In Sooriyawewa.\n I'm Open University Student.\n I'm Software Engineering Student."
-        };
-        final int[] index = {0};
+        final ImageView homeImage = findViewById(R.id.homeImage);
+        final ImageView settingsImage = findViewById(R.id.settingsImage);
+        final ImageView notificationImage = findViewById(R.id.notificationImage);
 
-        Handler handler = new Handler();
-        Runnable runnable = new Runnable() {
+        final TextView homeTxt = findViewById(R.id.homeTxt);
+        final TextView settingsTxt = findViewById(R.id.settingsTxt);
+        final TextView notificationTxt = findViewById(R.id.notificationTxt);
+
+        // set Home Fragment as a default one...
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(R.id.fragmentContainer, HomeFragment.class,null)
+                .commit();
+        notificationLayout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                if (announcements.length==0){
-                    announcementBanner.setText("This Is Our Announcement Section\nAnd\nThis Time Not Have\nAnnouncements.");
-                }else {
-                    announcementBanner.setText(announcements[index[0]]);
-                    index[0] = (index[0] + 1) % announcements.length;
-                    handler.postDelayed(this, 4000); // Change every 4 seconds
+            public void onClick(View v) {
+                if(selectedTab !=2){
+
+//                    set notification Fragment ...
+
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.fragmentContainer, NotificationFragment.class,null)
+                            .commit();
+
+
+                    homeTxt.setVisibility(View.GONE);
+                    settingsTxt.setVisibility(View.GONE);
+
+                    homeImage.setImageResource(R.drawable.home);
+                    settingsImage.setImageResource(R.drawable.settings);
+
+                    homeLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    settingsLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+                    // Selected notification Tab...
+                    notificationTxt.setVisibility(View.VISIBLE);
+                    notificationImage.setImageResource(R.drawable.notification);
+                    notificationLayout.setBackgroundResource(R.drawable.round_bottom_back);
+
+                    //  create animation
+
+                    ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f,1.0f,1f,1f, Animation.RELATIVE_TO_SELF,1.0f,Animation.RELATIVE_TO_SELF,0.0f);
+                    scaleAnimation.setDuration(200);
+                    scaleAnimation.setFillAfter(true);
+                    notificationLayout.startAnimation(scaleAnimation);
+
+                    // set Middle tab as a Selected Tab...
+                    selectedTab =2;
                 }
             }
-        };
-        handler.post(runnable);
+        });
+
+        homeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(selectedTab !=1){
+
+//                    Set Home Fragment..
+
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.fragmentContainer, HomeFragment.class,null)
+                            .commit();
+
+                    notificationTxt.setVisibility(View.GONE);
+                    settingsTxt.setVisibility(View.GONE);
+
+                    notificationImage.setImageResource(R.drawable.notification);
+                    settingsImage.setImageResource(R.drawable.settings);
+
+                    notificationLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    settingsLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+                    // Selected Home Tab...
+                    homeTxt.setVisibility(View.VISIBLE);
+                    homeImage.setImageResource(R.drawable.home);
+                    homeLayout.setBackgroundResource(R.drawable.round_bottom_back);
+
+                    //  create animation
+
+                    ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f,1.0f,1f,1f, Animation.RELATIVE_TO_SELF,0.0f,Animation.RELATIVE_TO_SELF,0.0f);
+                    scaleAnimation.setDuration(200);
+                    scaleAnimation.setFillAfter(true);
+                    homeLayout.startAnimation(scaleAnimation);
+
+                    // set Middle tab as a Selected Tab...
+                    selectedTab =1;
+                }
+            }
+        });
+
+        settingsLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(selectedTab !=3){
+
+//                    set settings Fragments...
+
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.fragmentContainer, SettingsFragment.class,null)
+                            .commit();
+
+                    homeTxt.setVisibility(View.GONE);
+                    notificationTxt.setVisibility(View.GONE);
+
+                    homeImage.setImageResource(R.drawable.home);
+                    notificationImage.setImageResource(R.drawable.notification);
+
+                    homeLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                    notificationLayout.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+                    // Selected Settings Tab...
+                    settingsTxt.setVisibility(View.VISIBLE);
+                    settingsImage.setImageResource(R.drawable.settings);
+                    settingsLayout.setBackgroundResource(R.drawable.round_bottom_back);
+
+                    //  create animation
+
+                    ScaleAnimation scaleAnimation = new ScaleAnimation(0.8f,1.0f,1f,1f, Animation.RELATIVE_TO_SELF,1.0f,Animation.RELATIVE_TO_SELF,0.0f);
+                    scaleAnimation.setDuration(200);
+                    scaleAnimation.setFillAfter(true);
+                    settingsLayout.startAnimation(scaleAnimation);
+
+                    // set Settings tab as a Selected Tab...
+                    selectedTab =3;
+                }
+            }
+        });
     }
 }
