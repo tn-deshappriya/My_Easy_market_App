a13d81e4643e8aaa4b2ba83bb4021e7653cb4fad
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-08-21 15:27:05 +0530

    Completed:-Complete Createmarket UI section and navigations to Dashboard and not database connect yet.

--- app/src/main/java/com/s23010467/easy_market/CreateMarket.java
+++ app/src/main/java/com/s23010467/easy_market/CreateMarket.java
@@ -1,15 +1,25 @@
 package com.s23010467.easy_market;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CreateMarket extends AppCompatActivity {
+
+    CardView create_market_btn;
+    EditText Market_Name,Market_Address,Market_Contact;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +32,23 @@ public class CreateMarket extends AppCompatActivity {
         });
 
         Spinner spinner = findViewById(R.id.spinnerCategory);
+        Market_Name = findViewById(R.id.market_name);
+        Market_Address = findViewById(R.id.market_address);
+        Market_Contact = findViewById(R.id.market_contact);
+
+        create_market_btn = findViewById(R.id.create_market_btn);
+        create_market_btn.setOnClickListener( v -> {
+            String market_name = Market_Name.getText().toString().trim();
+            String market_address = Market_Address.getText().toString().trim();
+            String market_contact = Market_Contact.getText().toString().trim();
+            String market_category = spinner.getSelectedItem().toString();
+            if(market_name.isEmpty()||market_address.isEmpty()||market_contact.isEmpty()||market_category.equals("Select Category")){
+                Toast.makeText(CreateMarket.this,"All Feilds Are Required To Register!",Toast.LENGTH_SHORT).show();
+            }else {
+                Intent intent = new Intent(CreateMarket.this, dashboard.class);
+                startActivity(intent);
+            }
+        });
 
 // Create adapter using custom layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
